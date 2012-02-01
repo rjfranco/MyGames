5ccf4a3d1d0fe8aab5b22d94c184a6d4272adb05
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-02-01 08:14:54 -0800

    Adding files related tot he previous migration / game object creation

---
+++ db/migrate/20120201160627_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.integer :rating
+      t.string :comment
+      t.time :release
+
+      t.timestamps
+    end
+  end
+end
