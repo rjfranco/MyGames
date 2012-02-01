c60dac082467aa658935882b5020aeb829ee9a5b
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-02-01 07:44:58 -0800

    Adding initial files to repository

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Games
+## A Quick Test App for Ember / Rails
+
+This is really just a simple application for testing out the current build of the Ember.js framework ( 1.0 - Beta 2 ). What I intend to do is a create a view with a list of games that should update dynamically when a seperate client connects and makes a change.
+
+Questions on how this turns out? Ask me at [rjfranco@gmail.com](mailto:rjfranco@gmail.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyGames::Application.config.secret_token = '48b02427cd33758a6852d99bfbee55984b9085638879c9668f3d9c429753da50325692f53bcd8b12d43131e628f29a4e1befcbc9eb4515ebc8ca2bc9d6bdd1de'
